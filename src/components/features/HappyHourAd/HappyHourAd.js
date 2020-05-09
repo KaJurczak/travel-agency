8e36a016dbcb77d50b4175d745dfdce191cecbd6
Karolina Jurczak <inka_c@tlen.pl>
2020-05-09 14:55:06 +0200

    Add HappyHoutsAd tests - 1,2 and 3

---
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './TripListOptions.scss';
+// import {Row, Col} from 'react-flexbox-grid';
+
+class HappyHourAd extends React.Component {
+  render(){
+
+    const {title, promoDescription} = this.props;
+
+    return(
+      <div>
+        <h3 className={styles.title}>{title}</h3>
+        <div className={styles.promoDescription}>{promoDescription}</div>
+      </div>
+    );
+  }
+
+}
+
+HappyHourAd.propTypes = {
+  title: PropTypes.string,
+  promoDescription: PropTypes.string,
+};
+
+export default HappyHourAd;
